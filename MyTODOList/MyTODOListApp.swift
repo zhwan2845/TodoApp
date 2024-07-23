bfaadb52d0c18837b84e803acb9bf9e0ae1954e0
zhwan2845 <johaeun0802@naver.com>
2024-07-23 21:26:33 +0900

    Initial Commit

---
+++ MyTODOList/MyTODOListApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyTODOListApp.swift
+//  MyTODOList
+//
+//  Created by 조하은 on 7/23/24.
+//
+
+import SwiftUI
+
+@main
+struct MyTODOListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
